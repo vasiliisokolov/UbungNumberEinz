f108fa0ed36e1e14c611ef21458f79b3f43b22ed
Vasily Sokolov <vasilysokolov@mail.ru>
2022-09-07 18:43:27 +0300

    idk

--- UbungNumberEinz/UbungNumberEinz.cpp
+++ UbungNumberEinz/UbungNumberEinz.cpp
@@ -1,7 +1,35 @@
 ﻿#include <iostream>
 #include <vector>
 
+std::vector<int> replace(std::vector<int>);
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n, remove;
+    std::cout << "Enter vector  size!" << std::endl;
+    std::cin >> n;
+    std::cout << "Fill the vector!" << std::endl;
+    std::vector<int> vec(n);
+    for (int i = 0; i < vec.size(); i++)
+    {
+        std::cin >> vec[i];
+    }
+    std::cout << "Enter the value to be removed:\n";
+    std::cin >> remove;
+    
+
+
+}
+
+std::vector<int> replace(std::vector<int> vec)
+{
+    for (int i = vec.size() - 1; i != 0; i--)
+    {
+        if()
+        vec.pop_back();
+        
+    }
+
+
+        return vec;
 }
